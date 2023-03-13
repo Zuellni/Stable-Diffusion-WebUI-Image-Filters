111e584629b6878d7eead3dd05337627c4db0f57
Zuellni <123005779+Zuellni@users.noreply.github.com>
2023-03-14 00:00:10 +0100

    Store defaults as variables

--- scripts/image_filters.py
+++ scripts/image_filters.py
@@ -5,6 +5,12 @@ import pilgram
 
 script_name = "Image Filters"
 script_order = 100000
+val_min_enh = -10
+val_min = 0
+val_def = 0
+val_step = 1
+val_max = 10
+val_max_ops = 50
 
 def ui(is_extra):
 	args_main = {}
@@ -31,20 +37,20 @@ def ui(is_extra):
 					"equalize", "flip", "grayscale", "invert", "mirror",
 				])
 			with gr.Row():
-				args["ops_cutoff_low"] = gr.Slider(label = "Auto Contrast Low", minimum = 0, step = 1, maximum = 50, value = 0)
-				args["ops_cutoff_high"] = gr.Slider(label = "Auto Contrast High", minimum = 0, step = 1, maximum = 50, value = 0)
-				args["filter_box_blur"] = gr.Slider(label = "Box Blur", minimum = 0, step = 1, maximum = 10, value = 0)
-				args["filter_gaussian_blur"] = gr.Slider(label = "Gaussian Blur", minimum = 0, step = 1, maximum = 10, value = 0)
+				args["ops_cutoff_low"] = gr.Slider(label = "Auto Contrast Low", minimum = val_min, step = val_step, maximum = val_max_ops, value = val_def)
+				args["ops_cutoff_high"] = gr.Slider(label = "Auto Contrast High", minimum = val_min, step = val_step, maximum = val_max_ops, value = val_def)
+				args["filter_box_blur"] = gr.Slider(label = "Box Blur", minimum = val_min, step = val_step, maximum = val_max, value = val_def)
+				args["filter_gaussian_blur"] = gr.Slider(label = "Gaussian Blur", minimum = val_min, step = val_step, maximum = val_max, value = val_def)
 			with gr.Row():
-				args["filter_min"] = gr.Slider(label = "Min Filter", minimum = 0, step = 1, maximum = 10, value = 0)
-				args["filter_median"] = gr.Slider(label = "Median Filter", minimum = 0, step = 1, maximum = 10, value = 0)
-				args["filter_max"] = gr.Slider(label = "Max Filter", minimum = 0, step = 1, maximum = 10, value = 0)
-				args["filter_mode"] = gr.Slider(label = "Mode Filter", minimum = 0, step = 1, maximum = 10, value = 0)
+				args["filter_min"] = gr.Slider(label = "Min Filter", minimum = val_min, step = val_step, maximum = val_max, value = val_def)
+				args["filter_median"] = gr.Slider(label = "Median Filter", minimum = val_min, step = val_step, maximum = val_max, value = val_def)
+				args["filter_max"] = gr.Slider(label = "Max Filter", minimum = val_min, step = val_step, maximum = val_max, value = val_def)
+				args["filter_mode"] = gr.Slider(label = "Mode Filter", minimum = val_min, step = val_step, maximum = val_max, value = val_def)
 			with gr.Row():
-				args["enhance_brightness"] = gr.Slider(label = "Brightness", minimum = -10, step = 1, maximum = 10, value = 0)
-				args["enhance_color"] = gr.Slider(label = "Color", minimum = -10, step = 1, maximum = 10, value = 0)
-				args["enhance_contrast"] = gr.Slider(label = "Contrast", minimum = -10, step = 1, maximum = 10, value = 0)
-				args["enhance_sharpness"] = gr.Slider(label = "Sharpness", minimum = -10, step = 1, maximum = 10, value = 0)
+				args["enhance_brightness"] = gr.Slider(label = "Brightness", minimum = val_min_enh, step = val_step, maximum = val_max, value = val_def)
+				args["enhance_color"] = gr.Slider(label = "Color", minimum = val_min_enh, step = val_step, maximum = val_max, value = val_def)
+				args["enhance_contrast"] = gr.Slider(label = "Contrast", minimum = val_min_enh, step = val_step, maximum = val_max, value = val_def)
+				args["enhance_sharpness"] = gr.Slider(label = "Sharpness", minimum = val_min_enh, step = val_step, maximum = val_max, value = val_def)
 
 	list_main = list(args_main.values())
 	list_args = list(args.values())
@@ -52,7 +58,7 @@ def ui(is_extra):
 	return args_main | args if is_extra else list_main + list_args
 
 def reset(*args):
-	return [0 for arg in args]
+	return [val_def for arg in args]
 
 def map_filter(value):
 	return 1 + 2 * (value - 1)
@@ -78,21 +84,21 @@ def process(
 	for operation in operations:
 		pp.image = getattr(ImageOps, operation)(pp.image)
 
-	if ops_cutoff_low != 0 or ops_cutoff_high != 0:
+	if ops_cutoff_low != val_def or ops_cutoff_high != val_def:
 		pp.image = ImageOps.autocontrast(pp.image, (ops_cutoff_low, ops_cutoff_high), True)
 
-	if enhance_brightness != 0: pp.image = ImageEnhance.Brightness(pp.image).enhance(map_enhance(enhance_brightness))
-	if enhance_color != 0: pp.image = ImageEnhance.Color(pp.image).enhance(map_enhance(enhance_color))
-	if enhance_contrast != 0: pp.image = ImageEnhance.Contrast(pp.image).enhance(map_enhance(enhance_contrast))
-	if enhance_sharpness != 0: pp.image = ImageEnhance.Sharpness(pp.image).enhance(map_enhance(enhance_sharpness))
+	if enhance_brightness != val_def: pp.image = ImageEnhance.Brightness(pp.image).enhance(map_enhance(enhance_brightness))
+	if enhance_color != val_def: pp.image = ImageEnhance.Color(pp.image).enhance(map_enhance(enhance_color))
+	if enhance_contrast != val_def: pp.image = ImageEnhance.Contrast(pp.image).enhance(map_enhance(enhance_contrast))
+	if enhance_sharpness != val_def: pp.image = ImageEnhance.Sharpness(pp.image).enhance(map_enhance(enhance_sharpness))
 
-	if filter_box_blur != 0: pp.image = pp.image.filter(ImageFilter.BoxBlur(map_filter(filter_box_blur)))
-	if filter_gaussian_blur != 0: pp.image = pp.image.filter(ImageFilter.GaussianBlur(map_filter(filter_gaussian_blur)))
+	if filter_box_blur != val_def: pp.image = pp.image.filter(ImageFilter.BoxBlur(map_filter(filter_box_blur)))
+	if filter_gaussian_blur != val_def: pp.image = pp.image.filter(ImageFilter.GaussianBlur(map_filter(filter_gaussian_blur)))
 
-	if filter_min != 0: pp.image = pp.image.filter(ImageFilter.MinFilter(map_filter(filter_min)))
-	if filter_max != 0: pp.image = pp.image.filter(ImageFilter.MaxFilter(map_filter(filter_max)))
-	if filter_median != 0: pp.image = pp.image.filter(ImageFilter.MedianFilter(map_filter(filter_median)))
-	if filter_mode != 0: pp.image = pp.image.filter(ImageFilter.ModeFilter(map_filter(filter_mode)))
+	if filter_min != val_def: pp.image = pp.image.filter(ImageFilter.MinFilter(map_filter(filter_min)))
+	if filter_max != val_def: pp.image = pp.image.filter(ImageFilter.MaxFilter(map_filter(filter_max)))
+	if filter_median != val_def: pp.image = pp.image.filter(ImageFilter.MedianFilter(map_filter(filter_median)))
+	if filter_mode != val_def: pp.image = pp.image.filter(ImageFilter.ModeFilter(map_filter(filter_mode)))
 
 class Script(scripts.Script):
 	def title(self):
